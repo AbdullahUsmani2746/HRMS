dac8046385641c8f67328b7fabd0afbb428932ea
Abdullah Usmani <abdullah.usmani.2746@gmail.com>
2024-12-16 20:16:42 +0500

    commited

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGODB_URI, clientOptions);
         console.log('MONGODB_URI:', process.env.MONGODB_URI); // Temporary log
         await mongoose.connection.db.admin().command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Pinged your deployment. You are successfully connected to MongoDB!");
     } catch (error) {
         console.log('DB Connection Error:', error);
     }
